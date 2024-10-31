bd5218bd86adcfa42c9157647e0872a9696c2ee7
Scott Mudge <mail@scottmudge.com>
2024-10-30 21:48:30 -0400

    .

--- launc_char.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUT=$1
-if [ -z "$OUTPUT" ]; then
-    OUTPUT="C:\\Users\\Scott\\Desktop\\char"
-fi
-
-source ./venv/Scripts/activate
-python main.py --disable-metadata --use-pytorch-cross-attention --output-directory "${OUTPUT}\\output" --temp-directory "${OUTPUT}\\temp" --disable-xformers
